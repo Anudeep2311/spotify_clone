b5c22257f9e883135a89c33219198ac246868961
Anudeep2311 <anudeeplohghavkar2311@gmail.com>
2024-08-05 00:46:58 +0530

    Initial Commit

---
+++ lib/core/config/assets/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+
+  static const String introBG = '${basePath}intro_bg.png';
+  static const String chooseBg = '${basePath}choose_bg.png';
+  static const String rosBg = '${basePath}ros_bg.png';
+}
---
+++ lib/core/config/assets/app_vectors.dart
@@ -0,0 +1,10 @@
+class AppVectors {
+  static const String basePath = 'assets/vector/';
+  static const String format = '.svg';
+
+  static const String logo = "${basePath}spotify_logo$format";
+  static const String moon = "${basePath}moon$format";
+  static const String sun = "${basePath}sun$format";
+  static const String topPattern = "${basePath}ros1$format";
+  static const String bottomPattern = "${basePath}ros2$format";
+}
